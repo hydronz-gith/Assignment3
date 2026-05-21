9fd298a20d2826e1427884c03770a93dd9af6f65
105924231 <105924231@student.swin.edu.au>
2026-05-22 00:27:48 +1000

    completed basic mechanics
    some tweaks still needed, but majority is finished

---
+++ Assignment-3/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public static class EnemyAI
+{
+    private static readonly CombatHandler.CombatChoice[] choices =
+    {
+        CombatHandler.CombatChoice.Attack,
+        CombatHandler.CombatChoice.Block,
+        CombatHandler.CombatChoice.Dodge
+    };
+
+    public static CombatHandler.CombatChoice PickChoice()
+    {
+        return choices[Random.Range(0, choices.Length)];
+    }
+
+    public static CombatHandler.CombatChoice PickWeighted(float attackWeight, float blockWeight, float dodgeWeight)
+    {
+        float total = attackWeight + blockWeight + dodgeWeight;
+        float roll = Random.Range(0f, total);
+        if(roll < attackWeight) return CombatHandler.CombatChoice.Attack;
+        else if(roll < attackWeight + blockWeight) return CombatHandler.CombatChoice.Block;
+        else return CombatHandler.CombatChoice.Dodge;
+    }
+}
