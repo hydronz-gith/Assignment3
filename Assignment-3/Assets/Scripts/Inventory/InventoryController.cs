d8154424740723211a5e75be993e5e96b26d557a
hydronz-gith <hydronz15@gmail.com>
2026-06-06 14:47:51 +1000

    tweaks
    the script is being rude

--- Assignment-3/Assets/Scripts/Inventory/InventoryController.cs
+++ Assignment-3/Assets/Scripts/Inventory/InventoryController.cs
@@ -16,9 +16,6 @@ using UnityEngine;
 
         public List<InventoryItem> initialItems = new List<InventoryItem>();
 
-        [SerializeField]
-        private AudioClip dropClip;
-
         private void Start()
         {
             PrepareUI();
@@ -42,8 +39,8 @@ using UnityEngine;
             inventoryUI.ResetAllItems();
             foreach (var item in inventoryState)
             {
-                inventoryUI.UpdateData(item.Key, item.Value.item.ItemImage, 
-                    item.Value.quantity);
+    //            inventoryUI.UpdateData(item.Key, item.Value.item.ItemImage, 
+    //                item.Value.quantity);
             }
         }
 
@@ -51,8 +48,6 @@ using UnityEngine;
         {
             inventoryUI.InitializeInventoryUI(inventoryData.Size);
             inventoryUI.OnDescriptionRequested += HandleDescriptionRequest;
-            inventoryUI.OnSwapItems += HandleSwapItems;
-            inventoryUI.OnStartDragging += HandleDragging;
             inventoryUI.OnItemActionRequested += HandleItemActionRequest;
         }
 
@@ -62,27 +57,13 @@ using UnityEngine;
             if (inventoryItem.IsEmpty)
                 return;
 
-            IItemAction itemAction = inventoryItem.item as IItemAction;
-            if(itemAction != null)
-            {
-                
-                inventoryUI.ShowItemAction(itemIndex);
-                inventoryUI.AddAction(itemAction.ActionName, () => PerformAction(itemIndex));
-            }
-
-            IDestroyableItem destroyableItem = inventoryItem.item as IDestroyableItem;
-            if (destroyableItem != null)
-            {
-                inventoryUI.AddAction("Drop", () => DropItem(itemIndex, inventoryItem.quantity));
-            }
-
-        }
-
-        private void DropItem(int itemIndex, int quantity)
-        {
-            inventoryData.RemoveItem(itemIndex, quantity);
-            inventoryUI.ResetSelection();
-            audioSource.PlayOneShot(dropClip);
+            //IItemAction itemAction = inventoryItem.item as IItemAction;
+            //if(itemAction != null)
+            //{
+            //    
+            //    inventoryUI.ShowItemAction(itemIndex);
+            //    inventoryUI.AddAction(itemAction.ActionName, () => PerformAction(itemIndex));
+            //}
         }
 
         public void PerformAction(int itemIndex)
@@ -91,34 +72,21 @@ using UnityEngine;
             if (inventoryItem.IsEmpty)
                 return;
 
-            IDestroyableItem destroyableItem = inventoryItem.item as IDestroyableItem;
-            if (destroyableItem != null)
-            {
-                inventoryData.RemoveItem(itemIndex, 1);
+//            IDestroyableItem destroyableItem = inventoryItem.item as IDestroyableItem;
+//            if (destroyableItem != null)
+//            {
+//                inventoryData.RemoveItem(itemIndex, 1);
             }
 
-            IItemAction itemAction = inventoryItem.item as IItemAction;
-            if (itemAction != null)
-            {
-                itemAction.PerformAction(gameObject, inventoryItem.itemState);
-                audioSource.PlayOneShot(itemAction.actionSFX);
-                if (inventoryData.GetItemAt(itemIndex).IsEmpty)
-                    inventoryUI.ResetSelection();
-            }
-        }
-
-        private void HandleDragging(int itemIndex)
-        {
-            InventoryItem inventoryItem = inventoryData.GetItemAt(itemIndex);
-            if (inventoryItem.IsEmpty)
-                return;
-            inventoryUI.CreateDraggedItem(inventoryItem.item.ItemImage, inventoryItem.quantity);
-        }
-
-        private void HandleSwapItems(int itemIndex_1, int itemIndex_2)
-        {
-            inventoryData.SwapItems(itemIndex_1, itemIndex_2);
-        }
+//            IItemAction itemAction = inventoryItem.item as IItemAction;
+//            if (itemAction != null)
+//            {
+//                itemAction.PerformAction(gameObject, inventoryItem.itemState);
+//                audioSource.PlayOneShot(itemAction.actionSFX);
+//                if (inventoryData.GetItemAt(itemIndex).IsEmpty)
+//                    inventoryUI.ResetSelection();
+//            }
+//        }
 
         private void HandleDescriptionRequest(int itemIndex)
         {
@@ -129,25 +97,25 @@ using UnityEngine;
                 return;
             }
             ItemSO item = inventoryItem.item;
-            string description = PrepareDescription(inventoryItem);
-            inventoryUI.UpdateDescription(itemIndex, item.ItemImage,
-                item.name, description);
+            //string description = PrepareDescription(inventoryItem);
+            //inventoryUI.UpdateDescription(itemIndex, item.ItemImage,
+            //    item.name, description);
         }
 
-        private string PrepareDescription(InventoryItem inventoryItem)
-        {
-            StringBuilder sb = new StringBuilder();
-            sb.Append(inventoryItem.item.Description);
-            sb.AppendLine();
-            for (int i = 0; i < inventoryItem.itemState.Count; i++)
-            {
-                sb.Append($"{inventoryItem.itemState[i].itemParameter.ParameterName} " +
-                    $": {inventoryItem.itemState[i].value} / " +
-                    $"{inventoryItem.item.DefaultParametersList[i].value}");
-                sb.AppendLine();
-            }
-            return sb.ToString();
-        }
+//        private string PrepareDescription(InventoryItem inventoryItem)
+//        {
+//            StringBuilder sb = new StringBuilder();
+//            sb.Append(inventoryItem.item.Description);
+//            sb.AppendLine();
+//            for (int i = 0; i < inventoryItem.itemState.Count; i++)
+//            {
+//                sb.Append($"{inventoryItem.itemState[i].itemParameter.ParameterName} " +
+//                    $": {inventoryItem.itemState[i].value} / " +
+//                    $"{inventoryItem.item.DefaultParametersList[i].value}");
+//                sb.AppendLine();
+//            }
+ //           return sb.ToString();
+//        }
 
         public void Update()
         {
@@ -158,9 +126,9 @@ using UnityEngine;
                     inventoryUI.Show();
                     foreach (var item in inventoryData.GetCurrentInventoryState())
                     {
-                        inventoryUI.UpdateData(item.Key,
-                            item.Value.item.ItemImage,
-                            item.Value.quantity);
+                        //inventoryUI.UpdateData(item.Key,
+                           //item.Value.item.ItemImage,
+                            //item.Value.quantity);
                     }
                 }
                 else
